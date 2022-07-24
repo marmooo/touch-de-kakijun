25d2e0ed7b0565d9eb7805c5f3b316f407d05028
marmooo <marmooo@users.noreply.github.com>
2022-07-24 09:06:08 +0900

    bump bootstrap from 5.1.3 to 5.2.0

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-07-17 20:20";
+var CACHE_NAME = "2022-07-24 00:46";
 var urlsToCache = [
   "/touch-de-kakijun/",
   "/touch-de-kakijun/index.js",
@@ -9,7 +9,7 @@ var urlsToCache = [
   "/touch-de-kakijun/favicon/favicon.svg",
   "/kanjivg/09806.svg",
   "/kanjivg/06f22.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css",
 ];
 
 self.addEventListener("install", function (event) {
