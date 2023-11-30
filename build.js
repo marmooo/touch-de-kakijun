f59f690894a015b21989352b60e0ab14123dbe19
marmooo <marmooo@users.noreply.github.com>
2023-12-01 00:16:46 +0900

    improve

---
+++ build.js
@@ -0,0 +1,38 @@
+import { Onkun } from "https://raw.githubusercontent.com/marmooo/onkun/v0.2.6/mod.js";
+import { JKAT } from "npm:@marmooo/kanji@0.0.2";
+
+function getYomis(kanji, grade) {
+  const onkun = onkunDict.get(kanji);
+  if (grade <= 5) {
+    return onkun["小学"];
+  } else if (grade <= 7) {
+    const yomis = [];
+    yomis.push(...onkun["小学"]);
+    yomis.push(...onkun["中学"]);
+    return yomis;
+  } else if (grade <= 9) {
+    const yomis = [];
+    yomis.push(...onkun["小学"]);
+    yomis.push(...onkun["中学"]);
+    yomis.push(...onkun["高校"]);
+    return yomis;
+  } else if (onkun) {
+    return onkun["Unihan"];
+  } else {
+    console.log(`warning: ${kanji} onkun is undefined`);
+    return [];
+  }
+}
+
+const onkunDict = new Onkun();
+await onkunDict.fetchJoyo(
+  "https://raw.githubusercontent.com/marmooo/onkun/v0.2.6/data/joyo-2017.csv",
+);
+
+const result = [];
+for (let g = 0; g < 10; g++) {
+  JKAT[g].forEach((kanji) => {
+    result.push(`${kanji}\t${getYomis(kanji, g).join(",")}`);
+  });
+}
+Deno.writeTextFileSync("src/yomi.tsv", result.join("\n"));
