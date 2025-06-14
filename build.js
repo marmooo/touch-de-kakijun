e887620e0dd235290748e46204ee7dd3ebdd6058
marmooo <marmooo@users.noreply.github.com>
2025-06-14 17:34:44 +0900

    bump @marmooo/kanji from 0.0.8 to 0.1.1

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 import { Onkun } from "npm:onkun@0.3.0";
-import { JKAT } from "npm:@marmooo/kanji@0.0.8";
+import { JKAT } from "npm:@marmooo/kanji@0.1.1";
 
 function getYomis(kanji, grade) {
   const onkun = onkunDict.get(kanji);
